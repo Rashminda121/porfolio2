c5d023a2ae4eb9a748ee7e8385080c3042f73da2
Jayamuni Rashminda <rashminda121@gmail.com>
2024-04-15 12:31:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ let changeText = () => {
   // circle skill
 
   const circles = document.querySelectorAll('.circle');
-circles.forEach(elem => {
+  circles.forEach(elem => {
   var dots = elem.getAttribute("data-dots");
   var marked = elem.getAttribute("data-percent");
   var percent = Math.floor(dots * marked / 100);
@@ -66,3 +66,84 @@ circles.forEach(elem => {
     }
   
 });
+
+// mix it up portfolio section
+var mixer = mixitup('.portfolio-gallery');
+
+
+// active menu
+
+let menuli = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu() {
+  let len = section.length;
+  while(--len && window.scrollY + 97 < section[len].offsetTop){}
+  menuli.forEach (sec => sec.classList.remove("active"));
+  menuli[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+
+// sticky navbar
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", function() {
+  header.classList.toggle("sticky", window.scrolly > 50)
+})
+
+
+// toggle icon navbar
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+}
+
+window.onscroll = () => {
+  menuIcon.classList.remove("bx-x");
+  navlist.classList.remove("open");
+}
+
+// paralax
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-items");
+    } else {
+      entry.target.classList.remove("show-items");
+    }
+  });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=> observer.observe (el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe (el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe (el));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
